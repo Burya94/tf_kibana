872f659e3ce2ebda8f4658a4e4c3ee5b80fa4bab
Artem Buria <artem_buria@epam.com>
2017-08-07 16:57:53 +0300

    sec rules

--- main.tf
+++ main.tf
@@ -61,6 +61,18 @@ resource "aws_security_group" "kibana" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress{
+        from_port = 80
+        to_port   = 80
+        protocol  = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress{
+        from_port = 443
+        to_port   = 443
+        protocol  = "tcp"
+        cidr_blocks =["0.0.0.0/0"]
+    }
 
     ingress {
         from_port   = 9200
